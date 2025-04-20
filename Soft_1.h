8ff846982e3d854b0ea918ca34e2efe76b9d3261
Rasmus <rasmus_kildeberg@outlook.dk>
2025-04-20 15:17:18 +0200

    Arbejdet videre i header. GameMaster klassen

--- Soft_1.h
+++ Soft_1.h
@@ -1,9 +1,21 @@
 #ifndef SOFT_1.H
 #define SOFT_1.H
 #include <string>
+#include <vector>
 using namespace std;
 
+class GameMaster{
+public: 
+    GameMaster();
+    int startGame();
+    int newHero();
+    int loadHero();
+    ~GameMaster();
 
+private:
+    vector<Character> enemies;
+
+}
 
 
 #endif
