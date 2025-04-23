8dd16e709c5438064ecb094489e4b8f490b5bbcc
Rasmus <rasmus_kildeberg@outlook.dk>
2025-04-23 18:06:05 +0200

    Lavet så den ikke hopper ud til hovedmenuen efter hver kamp

--- main.cpp
+++ main.cpp
@@ -61,9 +61,9 @@ int main() {
         }
 
 
-        if (aktivHero != nullptr) {
+        while (aktivHero != nullptr) {
             aktivHero->setHp(10 + (aktivHero->getLevel() - 1) * 2);
-            // Vælg fjende
+            
             cout << "\n--- VÆLG EN FJENDE ---\n";
             const vector<Fjende>& fjender = gm.getFjender();
             for (size_t i = 0; i < fjender.size(); ++i) {
@@ -71,42 +71,53 @@ int main() {
                      << " (HP: " << fjender[i].getHP()
                      << ", Styrke: " << fjender[i].getStyrke() << ")\n";
             }
-
+        
             int fvalg;
             cout << "Indtast nummer på den fjende du vil kæmpe imod: ";
             cin >> fvalg;
-
+        
             if (fvalg >= 1 && fvalg <= fjender.size()) {
                 Fjende valgtFjende = fjender[fvalg - 1];
                 cout << "\nKAMP STARTER! " << aktivHero->getNavn() << " VS " << valgtFjende.getNavn() << "!\n";
-
-              
+        
                 while (aktivHero->getHP() > 0 && valgtFjende.getHP() > 0) {
-                    // Helt angriber
                     int skadeTilFjende = aktivHero->angreb();  
                     cout << aktivHero->getNavn() << " angriber " << valgtFjende.getNavn() << " med " << skadeTilFjende << " skade!" << endl;
-                    if (valgtFjende.mistHP(skadeTilFjende) == 0) {  
+                    if (valgtFjende.mistHP(skadeTilFjende) == 0) {
                         cout << valgtFjende.getNavn() << " er død!" << endl;
-                        break;  
+                        aktivHero->modtagXp(valgtFjende.givXp());
+                        break;
                     }
-                
-                    // Fjende angriber
+        
                     int skadeTilHero = valgtFjende.angreb();  
                     cout << valgtFjende.getNavn() << " angriber " << aktivHero->getNavn() << " med " << skadeTilHero << " skade!" << endl;
-                    if (aktivHero->mistHP(skadeTilHero) == 0) {  
+                    if (aktivHero->mistHP(skadeTilHero) == 0) {
                         cout << aktivHero->getNavn() << " er død!" << endl;
-                        break;  
+                        break;
                     }
                 }
-                // Slut på kamp
+        
                 if (aktivHero->getHP() > 0) {
                     cout << aktivHero->getNavn() << " vandt kampen!\n";
                 } else {
                     cout << valgtFjende.getNavn() << " vandt kampen!\n";
                 }
-         
-                aktivHero = nullptr;
             } else {
+                cout << "Ugyldigt valg af fjende.\n";
+            }
+        
+            // Spørg om man vil fortsætte eller afslutte
+            cout << "\nVil du kæmpe igen eller gå til hovedmenu?\n";
+            cout << "1. Kæmp igen\n";
+            cout << "2. Gå til hovedmenu\n";
+            int valg;
+            cin >> valg;
+            if (valg == 2) {
+                aktivHero = nullptr;
+            }
+        
+        
+            else {
                 cout << "Ugyldigt valg af fjende.\n";
             }
         }
